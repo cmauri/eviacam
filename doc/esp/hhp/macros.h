f58f6cfd738f4b4240898638045933cd01c46b9e
cesar <cesar@VULCANO.(none)>
2010-11-14 17:45:44 +0100

    Documentation reorganization

--- doc/esp/hhp/macros.h
+++
@@ -1,8 +0,0 @@
-#ifdef _FORMAT_SINGLE_FILE
-#define _HREF(topicfile,linkedtext) \
-  <a href=\"_HELP_FILE#topicfile\">linkedtext</a>
-#else
-#define _HREF(topicfile,linkedtext) \
-  <a href=\"topicfile.htm\">linkedtext</a>
-#endif
-
