3f0018277911b43ce1d89fabb56d1309562b061c
Cesar Mauri <cesar@crea>
2012-11-02 19:51:39 +0100

    creavision/ code cleanup. Removed most compilation warnings.

--- creavision/crvimage.cpp
+++ creavision/crvimage.cpp
@@ -44,6 +44,8 @@ CIplImage::CIplImage (int width, int height, int depth, const char *pColorOrder)
 	bool retval= Create (width, height, depth, pColorOrder);
 #ifndef NDEBUG
 	assert (retval);
+#else
+	(void) (retval);
 #endif
 }
 
